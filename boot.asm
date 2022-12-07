8a2a3fd0cbd6f97e6dcfebeda291ec08f7783f7e
Pr0xe <csd4557@csd.uoc.gr>
2022-12-07 12:03:25 +0200

    Setup Protected mode with setting up GDT and 32 bit memory

--- boot.asm
+++ boot.asm
@@ -1,4 +1,4 @@
-ORG 0
+ORG 0x7c00
 BITS 16
 _start:
 	jmp short start
@@ -6,58 +6,74 @@ _start:
 
 times 33 db 0
 
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+
 start:
-	jmp 0x7c0:step2
+	jmp 0:step2
 
 step2:
 	cli ; Clear Interrupts
-	mov ax, 0x7c0
+	mov ax, 0x00
 	mov ds, ax
 	mov es, ax
-	mov ax, 0x00
 	mov ss, ax
 	mov sp, 0x7c00
 	sti ; Enable Interrupts
 
-	mov ah, 2 ; Read sector command
-	mov al, 1 ; One sector to read
-	mov ch, 0 ; cylinder low eight bits
-	mov cl, 2 ; Read sector two
-	mov dh, 0 ; Head number
-	mov bx, buffer
-	int 0x13
-	jc error
+.load_protected:
+	cli
+	lgdt[gdt_descriptor]
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax
+	jmp CODE_SEG:load32
 
-	mov si, buffer
-	call print
 
-        jmp $
+;GDT
+gdt_start:
 
-error:
-	mov si, error_message
-	call print
-        jmp $
+gdt_null:
+	dd 0x0
+	dd 0x0
 
-print:
-        mov bx, 0
-.loop:
-        lodsb
-        cmp al, 0
-        je .done
-        call print_char
-        jmp .loop
+;offset 0x8
+gdt_code: 		; CS should point to this
+	dw 0xffff 	; Segment limit first 0-15 bits
+	dw 0 		; Base first 0-15 bits
+	db 0		; Base 16-23 bits
+	db 0x9a		; Access byte
+	db 11001111b	; High 4 bit flags and Low 4 bit flags
+	db 0 		; Base 24-31 bits
 
-.done:
-        ret
+;offset 0x10
+gdt_data:		; DS, SS, ES, FS, GS
+	dw 0xffff 	; CS should point to this
+	dw 0 		; Base first 0-15 bits
+	db 0		; Base 16-23 bits
+	db 0x92		; Access byte
+	db 11001111b	; High 4 bit flags and Low 4 bit flags
+	db 0 		; Base 24-31 bits
 
-print_char:
-        mov ah, 0eh
-        int 0x10
-        ret
+gdt_end:
 
-error_message: db 'Failed to load sector' , 0
+gdt_descriptor:
+	dw gdt_end - gdt_start-1
+	dd gdt_start
+
+[BITS 32]
+load32:
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	mov ebp, 0x00200000
+	mov esp, ebp
+	jmp $
 
 times 510-($ -$$) db 0
 dw 0xAA55
 
-buffer:
