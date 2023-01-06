977fdceaa7ee6e29f075ac3a64ba4d0d69b5cc8c
Pr0xe <csd4557@csd.uoc.gr>
2023-01-06 19:36:30 +0200

    GDT packed

--- src/gdt/gdt.h
+++ src/gdt/gdt.h
@@ -3,22 +3,24 @@
 
 #include <stdint.h>
 
-struct gdt{
-        uint16_t segment;
-        uint16_t base_first;
-        uint8_t base;
-        uint8_t access;
-        uint8_t high_flags;
-        uint8_t base_24_31_bits;
+struct gdt {
+	uint16_t segment;
+	uint16_t base_first;
+	uint8_t base;
+	uint8_t access;
+	uint8_t high_flags;
+	uint8_t base_24_31_bits;
+} __attribute__((packed));
+
+struct gdt_structured {
+	uint32_t base;
+	uint32_t limit;
+	uint8_t type;
 };
 
-struct gdt_structured{
-        uint32_t base;
-        uint32_t limit;
-        uint8_t type;
-};
-
-void gdt_load(struct gdt* gdt, int size);
-void gdt_structured_to_gdt(struct gdt* gdt, struct gdt_structured* structure_gdt, int total_entries);
+void gdt_load(struct gdt *gdt, int size);
+void gdt_structured_to_gdt(struct gdt *gdt,
+			   struct gdt_structured *structure_gdt,
+			   int total_entries);
 
 #endif
