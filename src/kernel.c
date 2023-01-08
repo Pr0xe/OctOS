4a84f3a9e9f5d4e4e9af5b1ad28ce2cdba678dbe
Pr0xe <csd4557@csd.uoc.gr>
2023-01-09 00:50:19 +0200

    creating kernel command

--- src/kernel.c
+++ src/kernel.c
@@ -17,6 +17,7 @@
 #include "task/task.h"
 #include "task/process.h"
 #include "status.h"
+#include "isr80h/isr80h.h"
 
 uint16_t *video_mem = 0;
 uint16_t terminal_row = 0;
@@ -130,6 +131,9 @@ void kernel_main()
 
 	//Enable Paging
 	enable_paging();
+
+	//register kernel commands
+	isr80h_register_commands();
 
 	struct process *process = 0;
 	int res = process_load("0:/blank.bin", &process);
