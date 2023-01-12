cafd21ab480784afc6d9475362df09d5c0d542ea
Pr0xe <csd4557@csd.uoc.gr>
2023-01-12 21:41:49 +0200

    Improving keyboard driver

--- src/keyboard/classic.c
+++ src/keyboard/classic.c
@@ -2,9 +2,13 @@
 #include "keyboard.h"
 #include <stdint.h>
 #include "io/io.h"
+#include "kernel.h"
+#include "idt/idt.h"
 #include <stddef.h>
+#include "task/task.h"
 
 int classic_keyboard_init();
+void classic_keyboard_handle_interrupt();
 
 static uint8_t keyboard_scan_set_one[] = {
 	0x00, 0x1B, '1',  '2',	'3',  '4',  '5',  '6',	'7',  '8',  '9',  '0',
@@ -21,6 +25,8 @@ struct keyboard classic_keyboard = { .name = { "Classic" },
 
 int classic_keyboard_init()
 {
+	idt_register_interrupt_callback(ISR_KEYBOARD_INTERRUPT,
+					classic_keyboard_handle_interrupt);
 	outb(PS2_PORT, PS2_COMMAND_ENABLE_FIRST_PORT);
 	return 0;
 }
@@ -39,6 +45,21 @@ uint8_t classic_keyboard_scancode_to_char(uint8_t scancode)
 
 void classic_keyboard_handle_interrupt()
 {
+	kernel_page();
+	uint8_t scancode = 0;
+	scancode = insb(KEYBOARD_INPUT_PORT);
+	insb(KEYBOARD_INPUT_PORT);
+
+	if (scancode & CLASSIC_KEYBOARD_KEY_RELEASED) {
+		return;
+	}
+
+	uint8_t c = classic_keyboard_scancode_to_char(scancode);
+	if (c != 0) {
+		keyboard_push(c);
+	}
+
+	task_page();
 }
 
 struct keyboard *classic_init()
