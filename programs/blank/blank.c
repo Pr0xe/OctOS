171152932457cc1aad256a19296ca9c099a9a3df
Pr0xe <csd4557@csd.uoc.gr>
2023-01-14 15:33:10 +0200

    Implementing stdlib print function

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,4 +1,9 @@
+#include "octos.h"
+
 int main(int argc, char **argv)
 {
+	print("Hello this is test\n");
+	while (1) {
+	}
 	return 0;
 }
---
+++ programs/stdlib/src/octos.h
@@ -0,0 +1,6 @@
+#ifndef OCTOS_H
+#define OCTOS_H
+
+void print(const char *filename);
+
+#endif
