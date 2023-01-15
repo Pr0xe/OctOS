5182b77e81a4f2385ac423fc84523f18de5bf109
Pr0xe <csd4557@csd.uoc.gr>
2023-01-15 19:57:52 +0200

    Creating a shell for the kernel

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -5,18 +5,6 @@
 int main(int argc, char **argv)
 {
 	printf("My age is %i\n", 23);
-	print("Hello this is test\n");
-
-	print(itoa(8763));
-
-	putchar('Z');
-
-	void *ptr = malloc(512);
-	free(ptr);
-
-	char buf[1024];
-	octos_terminal_readline(buf, sizeof(buf), true);
-	print(buf);
 
 	while (1) {
 	}
---
+++ programs/shell/src/shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "octos.h"
+
+int main(int argc, char **argv)
+{
+	print("OctOS Terminal\n");
+	while (1) {
+		print("->");
+		char buf[1024];
+		octos_terminal_readline(buf, sizeof(buf), true);
+		print("\n");
+	}
+	return 0;
+}
